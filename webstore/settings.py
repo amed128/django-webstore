c552583b04798ab5725e86409542f6fa4cc95b7f
Mou Niang <amednian@hotmail.fr>
2021-11-25 22:23:08 +0100

    updated

--- webstore/settings.py
+++ webstore/settings.py
@@ -145,8 +145,8 @@ if os.environ.get('ENV') == 'PRODUCTION':
     )
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-#     db_from_env = dj_database_url.config(conn_max_age=500)
-#     DATABASES['default'].update(db_from_env)
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static",]
