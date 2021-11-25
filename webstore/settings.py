540e8faf7a62b71d20efc997c17a4a3de7addbdf
Mou Niang <amednian@hotmail.fr>
2021-11-25 22:17:01 +0100

    updated

--- webstore/settings.py
+++ webstore/settings.py
@@ -137,12 +137,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# if os.environ.get('ENV') == 'PRODUCTION':
-#     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-#     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') 
-#     STATICFILES_DIRS = (
-#         os.path.join(PROJECT_ROOT, 'static'),
-#     )
+if os.environ.get('ENV') == 'PRODUCTION':
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') 
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
 #     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #     db_from_env = dj_database_url.config(conn_max_age=500)
