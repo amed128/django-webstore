569e23443f774609793e3e76901235e6b5463f19
Mou Niang <amednian@hotmail.fr>
2021-12-03 18:00:56 +0100

    updated

--- static/base.js
+++ static/base.js
@@ -1,6 +1,8 @@
 const nav = document.querySelector('.navbar');
 let navTop = nav.offsetTop;
 
+const  goUpBtn = document.querySelector('.go-up-btn')
+
 function fixedNav() {
   if (window.scrollY > navTop) {    
     nav.classList.add('fixed');
@@ -9,4 +11,28 @@ function fixedNav() {
   }
 }
 
+function upBttn() {
+  if (scrollY < 500) {
+    goUpBtn.hidden = true;
+  } else {
+    goUpBtn.hidden = false;
+    /* setTimeout(function () {
+      goUpBtn.hidden = true;
+    }, 3000) */
+  }
+
+  goUpBtn.addEventListener('click', function () {
+    // console.log('go up click')
+    if (scrollY) {
+      window.scroll(0,0);
+    }
+  });
+}
+
+
+
 window.addEventListener('scroll', fixedNav);
+window.addEventListener('scroll', upBttn);
+
+
+
